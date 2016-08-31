ed554bf1bb813384d64c2ea8f7d695bfa01db4d1
francesco-strazzullo <me@francescostrazzullo.info>
2016-08-31 09:26:16 +0200

    cosmetics

--- AngularJS/src/controller/RootController.js
+++ AngularJS/src/controller/RootController.js
@@ -3,9 +3,6 @@ import _ from 'lodash';
 
 export default function(AppState,Users) {
 
-    //Two way binding to the state...bad?
-    //$scope.state = AppState
-
     const selector = () => {
         const scopeData = _.pick(AppState,'users','numberOfUsers');
         _.extend(this,scopeData);
