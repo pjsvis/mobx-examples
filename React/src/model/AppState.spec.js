1164a57365d5f3234e4ed847a84d8cbc751de6d4
francesco-strazzullo <me@francescostrazzullo.info>
2016-09-14 17:33:03 +0200

    fixed unit test

--- React/src/model/AppState.spec.js
+++ React/src/model/AppState.spec.js
@@ -1,9 +1,8 @@
 import {assert} from 'chai';
-import AppState from "./AppState";
+import state from "./AppState";
 
 describe('AppState', function () {
     it('should sync users and numberOfUsers', function () {
-        const state = new AppState();
         state.users = [1,2,3];
         assert.equal(state.users.length,state.numberOfUsers);
     });
