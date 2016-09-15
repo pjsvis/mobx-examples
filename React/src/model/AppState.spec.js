5c7b97a68b85bcf05f2781fd3d6eb3eea5662893
francesco-strazzullo <me@francescostrazzullo.info>
2016-09-15 10:18:42 +0200

    fixed test

--- React/src/model/AppState.spec.js
+++ React/src/model/AppState.spec.js
@@ -1,9 +1,25 @@
 import {assert} from 'chai';
-import state from "./AppState";
+import {appState as state} from "./AppState";
 
 describe('AppState', function () {
-    it('should sync users and numberOfUsers', function () {
+    it('should calculate the length of the users in a getter', function () {
         state.users = [1,2,3];
         assert.equal(state.users.length,state.numberOfUsers);
+    });
+
+    it('should sync users and numberOfUsers', function () {
+        state.users = [
+            {
+                name:'Francesco',
+                gender:'male'
+            },
+            {
+                name:'Lucia',
+                gender:'female'
+            }
+        ];
+
+        assert.deepEqual(state.males,[state.users[0]]);
+        assert.deepEqual(state.females,[state.users[1]]);
     });
 });
